2664456db4bf8af50e96b2126950c0077e5615f0
Samuel Osoba <samuelsontech@gmail.com>
2023-02-17 18:51:40 +0100

    Functions Module

---
+++ functions.js
@@ -0,0 +1,13 @@
+// // for loops
+
+// for(let i=0; i < 5; I++) {
+//     console.log('Loop Output =', i);
+// }
+
+// Functions @ AppAcademy.io
+
+function average(number1, number2) {
+    return (number1 * number2) / 2
+}
+
+console.log(average(4, 5))
--- javascript-functions.js
+++
@@ -1,7 +0,0 @@
-// // for loops
-
-// for(let i=0; i < 5; I++) {
-//     console.log('Loop Output =', i);
-// }
-
-// Functions @ AppAcademy.io
