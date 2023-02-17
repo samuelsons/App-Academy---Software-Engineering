0c4a7df9f65797dce3f91bea28982814eb7639e5
Samuel Osoba <samuelsontech@gmail.com>
2023-02-17 19:12:48 +0100

    Understanding functions

--- functions.js
+++ functions.js
@@ -6,10 +6,21 @@
 
 // Functions @ AppAcademy.io
 
-function average(number1, number2) {
-    return (number1 + number2) / 2 //number1 is parameter.
+// function average(number1, number2) {
+//     return (number1 + number2) / 2 //number1 is parameter.
+// }
+
+// // Syntax is a Function call
+// console.log(average(4, 12)) // Num 4 * 12 are arguments.
+
+console.log("First!")
+
+function callMe() {
+    console.log("Second!")
+    console.log("Third!")
 }
 
-// Syntax is a Function call
-console.log(average(4, 12)) // Num 4 * 12 are arguments.
+callMe()
 
+
+console.log("Fourth!")
